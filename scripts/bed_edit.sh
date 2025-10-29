7e1bdb7693529a64f426f7994b6969a0107c1fc9
TrinityConn <trinityleia@gmail.com>
2025-10-29 10:40:25 -0500

    add 200kb het

--- scripts/bed_edit.sh
+++
@@ -1,8 +0,0 @@
-#take track id from bed file and make it a column for plotting in R 
-
-awk '
-BEGIN { track_line = "" }
-/^track/ { track_line = $0; next }
-/^browser/ { next }
-{ print $0 "\t" track_line }
-' acerroh.roh.bed > acerroh.track.bed
